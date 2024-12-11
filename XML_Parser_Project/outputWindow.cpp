c20f3d4eb051d2d33cc1d5c9f086735161cb0241
Mark Magdy <mark.anis12@gmail.com>
2024-12-11 11:24:22 +0200

    GUI V1.0

--- XML_Parser_Project/outputWindow.cpp
+++ XML_Parser_Project/outputWindow.cpp
@@ -4,11 +4,16 @@ outputWindow::outputWindow(QWidget* parent)
     : QWidget(parent) {
     outputEdit = new QPlainTextEdit(this);
     outputEdit->setReadOnly(true);  // Make the text output field read-only
-
+    outputEdit->setPlaceholderText("OUTPUT");
+    QFont font;
+    font.setFamily("Courier");
+    font.setFixedPitch(true);
+    font.setPointSize(10);
+    outputEdit->setFont(font);
     QVBoxLayout* layout = new QVBoxLayout(this);
     layout->addWidget(outputEdit);
     layout->addWidget(saveBtn);
-
+    
     setLayout(layout);
 
     //setWindowTitle("Output Code");
