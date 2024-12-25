8bf5e0472aeaa6285b074f9de3b21207543d1ee5
Ramez Emad <167022000+Rozza2@users.noreply.github.com>
2024-12-26 00:15:06 +0200

    Add files via upload

---
+++ Backend.h
@@ -0,0 +1,10 @@
+#ifndef Backend_H
+#define Backend_H
+
+#include "ComDec/lz77_compression.h"
+#include "XML2tree/XMLtoTree.h"
+#include "validation/validate.h"
+#include "beauty/beautify.h"
+ 
+
+#endif
