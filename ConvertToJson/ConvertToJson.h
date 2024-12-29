428270f2e6da242b45bddc20e6a1fbd04b8c36bc
jessyfady <jessyfady@icloud.com>
2024-12-29 17:26:06 +0200

    ConvertToJson

---
+++ ConvertToJson/ConvertToJson.h
@@ -0,0 +1,19 @@
+//
+// Created by Jessica on 12/29/2024.
+//
+
+#ifndef CONVERTTOJSON_H
+#define CONVERTTOJSON_H
+
+#include "../XML2tree/XMLtoTree.h"
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <stack>
+#include <cctype>
+#include <algorithm>
+
+
+
+
+#endif //CONVERTTOJSON_H
