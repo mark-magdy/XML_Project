71b664999ae8120b46d3b6f03076351a89ab5a5d
Mark Magdy <mark.anis12@gmail.com>
2025-01-01 18:22:55 +0200

    commenting main

--- Network_Analysis/command_line.cpp
+++ Network_Analysis/command_line.cpp
@@ -165,106 +165,106 @@ vector<long> get_suggest(string xml, long ID)
 
 
 
-int main() {
-    std::string xml = R"(
-<users>
-    <user>
-        <id>1</id>
-        <name>Ahmed Ali</name>
-        <posts>
-            <post>
-                <body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </body>
-                <topics>
-                    <topic>economy</topic>
-                    <topic>finance</topic>
-                </topics>
-            </post>
-            <post>
-                <body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo onsequat.
-                </body>
-                <topics>
-                    <topic>solar_energy</topic>
-                </topics>
-            </post>
-        </posts>
-        <followers>
-            <follower>
-                <id>2</id>
-            </follower>
-            <follower>
-                <id>3</id>
-            </follower>
-            <follower>
-                <id>4</id>
-            </follower>
-            <follower>
-                <id>5</id>
-            </follower>
-        </followers>
-    </user>
-    <user>
-        <id>2</id>
-        <name>Yasser Ahmed</name>
-        <posts>
-            <post>
-                <body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </body>
-                <topics>
-                    <topic>education</topic>
-                </topics>
-            </post>
-        </posts>
-        <followers>
-            <follower>
-                <id>1</id>
-            </follower>
-            <follower>
-                <id>3</id>
-            </follower>
-            <follower>
-                <id>6</id>
-            </follower>
-            <follower>
-                <id>7</id>
-            </follower>
-        </followers>
-    </user>
-    <user>
-        <id>3</id>
-        <name>Mohamed Sherif</name>
-        <posts>
-            <post>
-                <body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </body>
-                <topics>
-                    <topic>sports</topic>
-                </topics>
-            </post>
-        </posts>
-        <followers>
-            <follower>
-                <id>1</id>
-            </follower>
-            <follower>
-                <id>2</id>
-            </follower>
-            <follower>
-                <id>4</id>
-            </follower>
-        </followers>
-    </user>
-</users>
-
-
-)";
-    //string outputFile="kiro.jpg";
-    //draw_graph(xml,outputFile);
-    for ( auto &i : search_by_word(xml,"consequat")){
-        cout << i<< "\n\n" ;
-    }
-}
+//int main() {
+//    std::string xml = R"(
+//<users>
+//    <user>
+//        <id>1</id>
+//        <name>Ahmed Ali</name>
+//        <posts>
+//            <post>
+//                <body>
+//                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+//                </body>
+//                <topics>
+//                    <topic>economy</topic>
+//                    <topic>finance</topic>
+//                </topics>
+//            </post>
+//            <post>
+//                <body>
+//                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo onsequat.
+//                </body>
+//                <topics>
+//                    <topic>solar_energy</topic>
+//                </topics>
+//            </post>
+//        </posts>
+//        <followers>
+//            <follower>
+//                <id>2</id>
+//            </follower>
+//            <follower>
+//                <id>3</id>
+//            </follower>
+//            <follower>
+//                <id>4</id>
+//            </follower>
+//            <follower>
+//                <id>5</id>
+//            </follower>
+//        </followers>
+//    </user>
+//    <user>
+//        <id>2</id>
+//        <name>Yasser Ahmed</name>
+//        <posts>
+//            <post>
+//                <body>
+//                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+//                </body>
+//                <topics>
+//                    <topic>education</topic>
+//                </topics>
+//            </post>
+//        </posts>
+//        <followers>
+//            <follower>
+//                <id>1</id>
+//            </follower>
+//            <follower>
+//                <id>3</id>
+//            </follower>
+//            <follower>
+//                <id>6</id>
+//            </follower>
+//            <follower>
+//                <id>7</id>
+//            </follower>
+//        </followers>
+//    </user>
+//    <user>
+//        <id>3</id>
+//        <name>Mohamed Sherif</name>
+//        <posts>
+//            <post>
+//                <body>
+//                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+//                </body>
+//                <topics>
+//                    <topic>sports</topic>
+//                </topics>
+//            </post>
+//        </posts>
+//        <followers>
+//            <follower>
+//                <id>1</id>
+//            </follower>
+//            <follower>
+//                <id>2</id>
+//            </follower>
+//            <follower>
+//                <id>4</id>
+//            </follower>
+//        </followers>
+//    </user>
+//</users>
+//
+//
+//)";
+//    //string outputFile="kiro.jpg";
+//    //draw_graph(xml,outputFile);
+//    for ( auto &i : search_by_word(xml,"consequat")){
+//        cout << i<< "\n\n" ;
+//    }
+//}
